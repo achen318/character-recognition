fa34393f95833abac8535435d366e490c7522398
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-05-14 11:47:49 -0400

    Convert image to 28x28 matrix

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import numpy as np
 import random
 from mnist import MNIST
 
-mndata = MNIST('data')
+mndata = MNIST("data")
 
 images, labels = mndata.load_training()
 # images, labels = mndata.load_testing()
 
-index = random.randrange(0, len(images)) 
-print(mndata.display(images[index]))
+X = np.array(random.choice(images)).reshape(28, 28)
+# P, D, Q = np.linalg.svd(X, full_matrices=False)
 
+print(X.view())
