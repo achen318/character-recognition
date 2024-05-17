bc92afa20529cdca8cd1af666e17ff38a05488d1
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-05-17 10:20:41 -0400

    Normalize data

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
 from matplotlib import pyplot as plt
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
 
 # Load the dataset
 (trainX, trainY), (testX, testY) = mnist.load_data()
 
-trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
-testX = testX.reshape((testX.shape[0], 28, 28, 1))
+# Normalize pixel data to be in [0, 1]
+trainX = trainX.astype("float32") / 255.0
+testX = testX.astype("float32") / 255.0
 
+# Convert integers to a 10-row binary vector
 trainY = to_categorical(trainY)
 testY = to_categorical(testY)
 
-print(trainX)
-
+# Create the CNN model
+model = Sequential(
+    []
+)
 # https://machinelearningmastery.com/how-to-develop-a-convolutional-neural-network-from-scratch-for-mnist-handwritten-digit-classification/
