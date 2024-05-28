0ac2fe3e3499672403a4fea5be4a36cd787b077b
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-05-28 01:03:29 -0400

    nn keras

--- models/neural_network.py
+++ models/neural_network.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 
 class NeuralNetwork(BaseModel):
     def __init__(self):
-        super().__init__("neural_network.model")
+        super().__init__("neural_network.keras")
 
     def train(self, trainX, trainY) -> None:
         try:
